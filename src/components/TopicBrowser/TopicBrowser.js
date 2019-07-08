f70cfa04d7fa03afb0d31819d8f56a0f67d8682d
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-07-08 17:28:43 -0600

    react-project-1

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class TopicBrowser extends Component {
+    render() {
+        return (
+            <p>Hola Mundo!</p>
+        )
+    }
+}
+
+
+
+
